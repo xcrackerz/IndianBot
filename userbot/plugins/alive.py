21667d9766d138a78b01f98e72d697e4adee7383
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-07-21 12:34:06 +0530

    Removed Unusual Imports

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,8 +1,6 @@
 """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
 import asyncio
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
